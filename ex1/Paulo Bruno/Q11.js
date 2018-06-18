c228413b74e2df5c946041cafe52b11c9ae6fb34
brunolima14k <brunolima14k@gmail.com>
2018-06-18 19:09:19 -0300

    Atualização
    Corrigindo a questão

--- ex1/Paulo Bruno/Q11.js
+++ ex1/Paulo Bruno/Q11.js
@@ -1,4 +1,6 @@
-function Trocar_cor(){
-  document.getElementById('btnVerde').style.backgroundColor = "green"
-  document.getElementById('btnAmarelo').style.backgroundColor = "yellow"
+function Trocar_Verde(){
+  document.body.style.backgroundColor = "green"
+}
+function Trocar_Amarelo(){
+  document.body.style.backgroundColor = "yellow"
 }
