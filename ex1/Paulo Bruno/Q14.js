a6e1eaa5628204c20f0223822875ccaf6db26b4c
brunolima14k <brunolima14k@gmail.com>
2018-06-20 20:06:38 -0300

    Adiconando Questões
    Adicionando da questão 12 a 17

---
+++ ex1/Paulo Bruno/Q14.js
@@ -0,0 +1,5 @@
+function potencia(){
+  const v1 = Number(document.getElementById('nmbValor1').value)
+  const v2 = Number(document.getElementById('nmbValor2').value)
+  document.getElementById('resultado').innerHTML = v1**v2
+}
