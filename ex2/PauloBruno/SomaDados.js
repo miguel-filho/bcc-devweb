6bf9e3b7a2f3d788654b36fbb4d3db8e94dd36ff
brunolima14k <brunolima14k@gmail.com>
2018-07-04 22:44:22 -0300

    Atualização
    Algumas modificações no codigo para funciona

--- ex2/PauloBruno/SomaDados.js
+++ ex2/PauloBruno/SomaDados.js
@@ -2,24 +2,23 @@ const request = require('request')
 const cheerio = require('cheerio')
 let soma_dados = 0
 
-function pegaDados(f) 
+function pegaDados(resultado_dados) 
 {
     request("https://www.random.org/dice/?num=10",function(erro, resposta, html){
         const $ = cheerio.load(html)
         let imagens = $('img')
-        let total = 0
         for (let index of Object.keys(imagens)) {
             let img = imagens[index]
             if(img.type == "tag"&& img.name=="img")
             {
                 soma_dados += parseInt(img.attribs.alt)
-                console.log(img.attribs.alt)
             }
             
         }
-        console.log(img.attribs.alt)
+        let retorno = resultado_dados(soma_dados)
+        if(retorno == true)
+            pegaDados(resultado_dados)
     })
-    
 }
 module.exports ={
     pegaDados
